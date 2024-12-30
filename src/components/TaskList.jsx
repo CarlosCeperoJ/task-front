b4804142b68a8a9c0ba3ec5713e626504fd36a2b
Carlos Cepero <112908189+CarlosCeperoJ@users.noreply.github.com>
2024-12-30 14:03:31 -0500

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -6,7 +6,7 @@ const TaskList = ({ tasks, toggleTaskCompletion, deleteTask }) => {
       {tasks.map(task => (
         <div key={task._id} className={`bg-white p-4 rounded-lg shadow-lg ${task.completed ? 'bg-green-100' : 'bg-red-100'}`}>
           <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
-            <div className="flex-1">
+            <div className="flex-1 mb-4 md:mb-0">
               <h2 className={`text-xl font-bold text-gray-800 break-words ${task.completed ? 'line-through' : ''}`}>
                 {task.completed ? <i className="fas fa-check-circle mr-2 text-green-500"></i> : <i className="fas fa-times-circle mr-2 text-red-500"></i>}
                 {task.title}
@@ -14,7 +14,7 @@ const TaskList = ({ tasks, toggleTaskCompletion, deleteTask }) => {
               <p className="text-gray-600 break-words">{task.description}</p>
               <p className="text-gray-500 text-sm">{new Date(task.createdAt).toLocaleString()}</p>
             </div>
-            <div className="flex space-x-2 mt-4 md:mt-0">
+            <div className="flex space-x-2">
               <button
                 className={`p-2 rounded-lg ${task.completed ? 'bg-yellow-500' : 'bg-green-500'} text-white`}
                 onClick={() => toggleTaskCompletion(task._id, task.completed)}
